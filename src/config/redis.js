71b64acf3c195d02c0c64eb55ae28b0cdb6df708
Lailatul Karomah <karomahlailatul@gmail.com>
2022-10-17 13:04:15 +0700

    troubleshoot on redis on deployment-5

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,20 +1,27 @@
 
 require("dotenv").config();
-const redis = require('@redis/client')
+// var redis = require('redis')
 // import redis from 'redis'
+// import { createClient } from 'redis';
+const { createClient } = require('redis');
+
 
     let hostname = process.env.REDIS_HOSTNAME;
     let port = process.env.REDIS_PORT;
     let password = process.env.REDIS_PASSWORD ;
 
 
-const client = redis.createClient({
+const client = createClient({
     url: `redis://default:${password}@${hostname}:${port}`
  });
 
 
-client.on('error', (err) => console.log('Redis Client Error', err));
 
-client.connect();
+ (async () => {
+    await client.connect();
+})();
+
+client.on('connect', () => console.log('::> Redis Client Connected'));
+client.on('error', (err) => console.log('<:: Redis Client Error', err));
 
 module.exports = client
