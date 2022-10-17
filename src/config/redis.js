fecc6506c234818e55c265e4cbb355032a2dc410
Lailatul Karomah <karomahlailatul@gmail.com>
2022-10-17 13:13:36 +0700

    troubleshoot on redis on deployment-6

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,9 +1,51 @@
 
+// require("dotenv").config();
+// // var redis = require('redis')
+// // import redis from 'redis'
+// // import { createClient } from 'redis';
+// const { createClient } = require('redis');
+
+
+//     let hostname = process.env.REDIS_HOSTNAME;
+//     let port = process.env.REDIS_PORT;
+//     let password = process.env.REDIS_PASSWORD ;
+
+
+// const client = createClient({
+//     url: `redis://default:${password}@${hostname}:${port}`
+//  });
+
+
+
+//  (async () => {
+//     await client.connect();
+// })();
+
+// client.on('connect', () => console.log('::> Redis Client Connected'));
+// client.on('error', (err) => console.log('<:: Redis Client Error', err));
+
+// module.exports = client
+
+
+
+
+
+
+
+
+
+
+
+
 require("dotenv").config();
 // var redis = require('redis')
 // import redis from 'redis'
 // import { createClient } from 'redis';
-const { createClient } = require('redis');
+
+const Redis = require("ioredis");
+
+// const { createClient } = require('redis');
+// const redis = new Redis();
 
 
     let hostname = process.env.REDIS_HOSTNAME;
@@ -11,17 +53,17 @@ const { createClient } = require('redis');
     let password = process.env.REDIS_PASSWORD ;
 
 
-const client = createClient({
-    url: `redis://default:${password}@${hostname}:${port}`
- });
+const client = new Redis(`redis://default:${password}@${hostname}:${port}`);
 
 
+// console.log(client)
 
- (async () => {
-    await client.connect();
-})();
+//  (async () => {
+//     await client.connect();
+// })();
 
 client.on('connect', () => console.log('::> Redis Client Connected'));
 client.on('error', (err) => console.log('<:: Redis Client Error', err));
 
 module.exports = client
+
